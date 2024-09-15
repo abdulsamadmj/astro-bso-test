1d8c7116946a0f1e2290457ef7420f5fbda35fa5
Abdul Samad M J <samadmj4@gmail.com>
2024-09-15 17:02:10 +0400

    made pwa, signup form under modifications

---
+++ public/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "your-cache-name-v1";
+const urlsToCache = [
+  "/",
+  "/styles/main.css",
+  "/scripts/main.js",
+  // Add other assets to cache here
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).then((fetchResponse) => {
+          if (event.request.method === "GET") {
+            const cacheResponse = caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, fetchResponse.clone());
+              return fetchResponse;
+            });
+            return cacheResponse;
+          }
+          return fetchResponse;
+        })
+      );
+    })
+  );
+});
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,10 @@
-export default {
-  routes: {
-    home: "/",
-    products: "/products",
-    cart: "/user/cart",
-  },
+export const strapi = {
+  BASE_URL: import.meta.env.STRAPI_BASE_URL,
+  SIGNUP_URL: import.meta.env.STRAPI_BASE_URL + "/api/auth/local/register",
+};
+export const routes = {
+  HOME: "/",
+  PRODUCTS: "/products",
+  CART: "/user/cart",
+  SIGNUP: "/signup",
 };
